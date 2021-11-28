5ce104a26cf84e29d32d7bc9757ae3fbbb66eebe
Kalynovavs <valentinka18.12.95@gmail.com>
2021-11-28 12:30:28 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int price = 19000;// хранение стоимости билета
+        int priceOfMilli = 20;// хранения количества рублей для одной бонусной милли
+        int result = price / priceOfMilli;// рассчет количества бонусных милль
+        System.out.println(result);
+
+    }
+}
